99bcaedc3e4aedbefab9ffef160cfdc8cf3c215c
martintrifunov <martintrifunov@proton.me>
2023-12-29 22:47:09 +0100

    TimerCircle componentization & code cleanup

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,25 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
-import SVG, { Circle } from "react-native-svg";
-import Animated, {
+import {
   useSharedValue,
   withTiming,
-  useAnimatedProps,
   cancelAnimation,
   Easing,
 } from "react-native-reanimated";
 import TimerCountDown from "./TimerCountDown";
 import TimerToggleButton from "./TimerToggleButton";
 import TimerCycles from "./TimerCycles";
+import TimerCircle from "./TimerCircle";
 
-const CIRCLE_COLOR_FOCUS = "#60B3FF";
-const CIRCLE_COLOR_BREAK = "#77D368";
-const CIRCLE_LENGTH = 750;
-const CIRCLE_RADIUS = CIRCLE_LENGTH / (2 * Math.PI);
 const FOCUS_TIME_MINUTES = 0.2 * 60 * 1000;
 const BREAK_TIME_MINUTES = 0.1 * 60 * 1000;
 const { width, height } = Dimensions.get("window");
-const AnimatedRing = Animated.createAnimatedComponent(Circle);
 
 //GONNA CLEAN UP THE CODE LATER DON'T MIND ME :)
 export default function Timer() {
@@ -76,50 +70,20 @@ export default function Timer() {
     }
   }, [timerCount]);
 
-  const animatedProps = useAnimatedProps(() => ({
-    strokeDashoffset: 1 - CIRCLE_LENGTH * progress.value,
-  }));
-
   return (
     <View style={styles.container}>
-      <SVG style={{ position: "absolute", bottom: 150 }}>
-        <Circle
-          cx={width / 2}
-          cy={height / 2}
-          r={CIRCLE_RADIUS + 15}
-          fill={"white"}
-          style={styles.circle}
-        />
-        {/* BACKGROUND CIRCLE */}
-        <Circle
-          cx={width / 2}
-          cy={height / 2}
-          r={CIRCLE_RADIUS}
-          stroke={timerMode === "Focus" ? CIRCLE_COLOR_FOCUS : CIRCLE_COLOR_BREAK}
-          strokeWidth={10}
-          fill={"none"}
-          opacity={0.2}
-          
-        />
-        {/* FOREGROUND CIRCLE */}
-        <AnimatedRing
-          cx={width / 2}
-          cy={height / 2}
-          r={CIRCLE_RADIUS}
-          stroke={timerMode === "Focus" ? CIRCLE_COLOR_FOCUS : CIRCLE_COLOR_BREAK}
-          strokeWidth={10}
-          fill={"none"}
-          strokeDasharray={CIRCLE_LENGTH}
-          strokeDashoffset={CIRCLE_LENGTH}
-          animatedProps={animatedProps}
-          strokeLinecap={"round"}
-          rotation={-90}
-          originX={width / 2}
-          originY={height / 2}
-        />
-      </SVG>
+      <TimerCircle
+        width={width}
+        height={height}
+        timerMode={timerMode}
+        progress={progress}
+      />
 
-      <TimerCycles timerMode={timerMode} focusCounter={focusCounter} breakCounter={breakCounter} />
+      <TimerCycles
+        timerMode={timerMode}
+        focusCounter={focusCounter}
+        breakCounter={breakCounter}
+      />
       <TimerCountDown timerDate={new Date(timerCount)} />
 
       <TimerToggleButton
@@ -145,14 +109,4 @@ const styles = StyleSheet.create({
     bottom: 140,
     color: "#535353",
   },
-  circle: {
-    shadowColor: "#333333",
-    shadowOffset: {
-      width: 6,
-      height: 6,
-    },
-    shadowOpacity: 0.6,
-    shadowRadius: 4,
-    elevation: 5
-  }
 });
---
+++ src/components/TimerCircle.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import SVG, { Circle } from "react-native-svg";
+import Animated, {
+  useAnimatedProps,
+} from "react-native-reanimated";
+
+const CIRCLE_COLOR_FOCUS = "#60B3FF";
+const CIRCLE_COLOR_BREAK = "#77D368";
+const CIRCLE_LENGTH = 750;
+const CIRCLE_RADIUS = CIRCLE_LENGTH / (2 * Math.PI);
+const AnimatedRing = Animated.createAnimatedComponent(Circle);
+
+const TimerCircle = ({ width, height, timerMode, progress }) => {
+  const animatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: 1 - CIRCLE_LENGTH * progress.value,
+  }));
+  return (
+    <SVG style={{ position: "absolute", bottom: 150 }}>
+      <Circle
+        cx={width / 2}
+        cy={height / 2}
+        r={CIRCLE_RADIUS + 15}
+        fill={"white"}
+      />
+      {/* BACKGROUND CIRCLE */}
+      <Circle
+        cx={width / 2}
+        cy={height / 2}
+        r={CIRCLE_RADIUS}
+        stroke={timerMode === "Focus" ? CIRCLE_COLOR_FOCUS : CIRCLE_COLOR_BREAK}
+        strokeWidth={10}
+        fill={"none"}
+        opacity={0.2}
+      />
+      {/* FOREGROUND CIRCLE */}
+      <AnimatedRing
+        cx={width / 2}
+        cy={height / 2}
+        r={CIRCLE_RADIUS}
+        stroke={timerMode === "Focus" ? CIRCLE_COLOR_FOCUS : CIRCLE_COLOR_BREAK}
+        strokeWidth={10}
+        fill={"none"}
+        strokeDasharray={CIRCLE_LENGTH}
+        strokeDashoffset={CIRCLE_LENGTH}
+        animatedProps={animatedProps}
+        strokeLinecap={"round"}
+        rotation={-90}
+        originX={width / 2}
+        originY={height / 2}
+      />
+    </SVG>
+  );
+};
+
+export default TimerCircle;
